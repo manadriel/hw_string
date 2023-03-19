320c92876d5a664182bbbcaad79b99831e579ead
manadriel <manadriel@yandex.ru>
2023-03-19 23:54:05 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
+        task3();
+
     }
+    public static void task1() { //name
+        System.out.println("Задача 1, 2");
+        String firstName = "Ivan " ;
+        String middleName = "Ivanovich " ;
+        String lastName = "Ivanov";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+    }
+
+    public static void task3() { //average
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
+    }
+
 }
